c7234c16b559a16b39ba68b3c05592bfec266d4a
Aarav Lodha <lodhaaarav12005@gmail.com>
2024-05-14 18:47:53 +0530

    modified the memory allocation code

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,142 +1,76 @@
-#include <stdio.h>
+#include<stdio.h>
 
-void firstFit(int bsize[], int psize[], int bno, int pno) 
+void firstfit(int bsize[], int psize[], int bno, int pno)
 {
-    int flags[10], allocation[10], i, j;
-
-    for (i = 0; i < 10; i++) 
+    int flag[bno];
+    for(int i=0;i<bno;i++)
     {
-        flags[i] = 0;
-        allocation[i] = -1;
+        flag[i]=0;
     }
-
-    for (i = 0; i < pno; i++) 
+    for(int i=0;i<pno;i++)
     {
-        for (j = 0; j < bno; j++) 
+        for(int j=0;j<bno;j++)
         {
-            if (flags[j] == 0 && bsize[j] >= psize[i]) 
+            if(bsize[j]>psize[i] && flag[j]==0)
             {
-                allocation[j] = i;
-                flags[j] = 1;
+                printf("Process %d is allocated to Block %d\n", i+1, j+1);
+                flag[j]=1;
                 break;
             }
-        }
-    }
-
-    printf("\nProcess no.\tBlock Size\tBlock No\tProcess Size");
-    for (i = 0; i < bno; i++) 
-    {
-        printf("\n%d\t\t%d\t\t", i + 1, bsize[i]);
-        if (flags[i] == 1)
-        {
-            printf("%d\t\t%d", allocation[i] + 1, psize[allocation[i]]);
-        }
-        else
-        {
-            printf("Not allocated\t-");
-        }
-    }
-}
-
-void bestFit(int bsize[], int psize[], int bno, int pno) {
-    int flags[15], allocation[15], i, j;
-
-    for (i = 0; i < 15; i++) 
-    {
-        flags[i] = 0;
-        allocation[i] = -1;
-    }
-    for (i = 0; i < pno; i++) 
-    {
-        int indexPlaced = -1;
-        for (j = 0; j < bno; j++) 
-        {
-            if (bsize[j] >= psize[i] && flags[j] == 0) 
+            if(j==bno-1)
             {
-                if (indexPlaced == -1)
-                {
-                    indexPlaced = j;
-                }
-                else if (bsize[j] < bsize[indexPlaced])
-                {
-                    indexPlaced = j;
-                }
+                printf("Process %d is not allocated\n", i+1);
             }
         }
-        if (indexPlaced != -1) 
-        {
-            allocation[i] = indexPlaced;
-            flags[indexPlaced] = 1;
-        }
-    }
-
-    printf("\nProcess No.\tMemory Size\tBlock No.\tProcess Size");
-    for (i = 0; i < pno; i++) 
-    {
-        printf("\n%d\t\t%d\t\t", i + 1, bsize[allocation[i]]);
-        if (allocation[i] != -1)
-        {
-            printf("%d\t\t%d", allocation[i] + 1, psize[i]);
-        }
-        else
-        {
-            printf("Not allocated");
-        }
     }
 }
 
-void worstFit(int bsize[], int psize[], int bno, int pno) 
+void bestFit(int bsize[], int psize[], int bno, int pno) 
 {
-    int allocation[15], i, j;
-
-    for (i = 0; i < pno; i++)
+    for(int i=0;i<pno;i++)
     {
-        allocation[i] = -1;
-    }
-        
-
-    for (i = 0; i < pno; i++) 
-    {
-        int indexPlaced = -1;
-        for (j = 0; j < bno; j++) 
+        int min=9999;
+        int index=-1;
+        for(int j=0;j<bno;j++)
         {
-            if (bsize[j] >= psize[i]) 
+            if(bsize[j]>=psize[i] && bsize[j]<min)
             {
-                if (indexPlaced == -1)
-                {
-                    indexPlaced = j;
-                }
-                else if (bsize[j] > bsize[indexPlaced])
-                {
-                    indexPlaced = j;
-                }
+                min=bsize[j];
+                index=j;
             }
         }
-        if (indexPlaced != -1) 
+        if(index!=-1)
         {
-            allocation[i] = indexPlaced;
-            bsize[indexPlaced] -= psize[i];
+            printf("Process %d is allocated to Block %d\n", i+1, index+1);
+        }
+        else
+        {
+            printf("Process %d is not allocated\n", i+1);
         }
     }
+}
 
-    printf("\nProcess No.\tMemory Size\tBlock No.\tProcess Size");
-    for (i = 0; i < pno; i++) 
+void worstfit(int bsize[], int psize[], int bno, int pno) 
+{
+    for(int i=0;i<pno;i++)
     {
-        if (bsize[allocation[i]] == -1)
+        int max=-1;
+        int index=-1;
+        for(int j=0;j<bno;j++)
         {
-            printf("\n%d\t\tNone\t\t", i + 1);
+            if(bsize[j]>=psize[i] && bsize[j]>max)
+            {
+                max=bsize[j];
+                index=j;
+            }
+        }
+        if(index!=-1)
+        {
+            printf("Process %d is allocated to Block %d\n", i+1, index+1);
         }
         else
         {
-            printf("\n%d\t\t%d\t\t", i + 1, bsize[allocation[i]]);
-        }
-        if (allocation[i] != -1)
-        {
-            printf("%d\t\t%d", allocation[i] + 1, psize[i]);
-        }
-        else
-        {
-            printf("Not allocated\t%d", psize[i]);
+            printf("Process %d is not allocated\n", i+1);
         }
     }
 }
@@ -144,7 +78,6 @@ void worstFit(int bsize[], int psize[], int bno, int pno)
 int main() 
 {
     int bsize[15], psize[15], bno, pno, choice, i;
-
     printf("Enter the number of memory blocks: ");
     scanf("%d", &bno);
 
@@ -163,7 +96,7 @@ int main()
         scanf("%d", &psize[i]);
     }
 
-    firstFit(bsize, psize, bno, pno);
+    firstfit(bsize, psize, bno, pno);
     bestFit(bsize, psize, bno, pno);
     worstFit(bsize, psize, bno, pno);
     return 0;
